bf6445db711760c0b3952b95ccf424682108f02e
meichone <cn.lorca@gmail.com>
2019-12-13 20:16:14 -0300

    instalacion de influxdb

--- influxdb.sh
+++ influxdb.sh
@@ -6,3 +6,8 @@ echo baseurl = https://repos.influxdata.com/rhel/\$releasever/\$basearch/stable 
 echo enabled = 1 >> /etc/yum.repos.d/influxdb.repo
 echo gpgcheck = 1 >> /etc/yum.repos.d/influxdb.repo
 echo gpgkey = https://repos.influxdata.com/influxdb.key >> /etc/yum.repos.d/influxdb.repo
+repolist >> /dev/null
+sudo yum install influxdb >> /dev/null
+sudo service influxdb start >> /dev/null
+read -n 1 -p "la Instalacion de influx ya finalizo, apreta una tecla para continuar"
+
